c91085a14ac64a9df5e993094ab75f8f61ecb5be
Tony Edwards <tony.edwards@gmail.com>
2022-04-16 00:22:20 +0100

    Refactors catching of birds to class
    Refactors the loading of bird data to sit within the bird class.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,4 @@
-const getBirds = async () => {
-    const response = await fetch("../js/birds.json");
-    const json = await response.json();
-    console.log(json);
-    return json;
-}
-
-let flock = [];
-
-getBirds().then(birds => {
-    birds.forEach(bird => {
-        console.log(bird);
-        console.log(bird.commonName)
-        flock[bird.commonName] = new Bird(bird.commonName, bird.latinName, bird.image, bird.song, bird.firstSighting);
-    });
-
+Bird.catchBirds().then(flock => {
     console.log('-----');
     console.log(flock);
     console.log(flock['Bird to the max']);
--- js/models/bird.js
+++ js/models/bird.js
@@ -8,15 +8,18 @@ class Bird {
       this.song = song;
       this.firstSighting = new Date(firstSighting);
 
-      console.log(this.commonName + " has been created");
+      console.log(this.commonName + " has been caught.");
     }
 
-    // static catchBirds(){
-    //     return fetch("../js/birds.json")
-    //         .then(response => response.json())
-    //         .then(json => {
-    //             console.log(json);
-    //             resolve(json);
-    //         });
-    // }
+    static catchBirds = async () => {
+        const response = await fetch("../js/birds.json");
+        const json = await response.json();
+        const flock = [];
+    
+        json.forEach(bird => {
+            flock[bird.commonName] = new Bird(bird.commonName, bird.latinName, bird.image, bird.song, bird.firstSighting);
+        });
+    
+        return flock;
+    }
   }
