628a187a06404271597d3a9ea0b23116699412bc
Tony Edwards <tony.edwards@gmail.com>
2022-05-19 15:36:50 +0100

    Adds nothing for a pair

--- js/cardGame.js
+++ js/cardGame.js
@@ -23,7 +23,7 @@ class PlayCardsGame extends Game {
     }
 
     processKeyPress(state, key, velocity){
-        console.log('Keypress received in game controller', key, state, velocity);
+        console.log('GAME: Keypress received: ', key, state, velocity);
 
         if ((key >= 48 && key <= 72) && state === 145 && !this.hasSelected){ // Keyboard/On
             this.hasSelected = true;
@@ -40,9 +40,7 @@ class PlayCardsGame extends Game {
                 app.songs[key].play();
             }
         } else if ((key >= 48 && key <= 72) && state === 129 && !this.hasSelected){ // Keyboard/Off
-            // stop bird singing
-            // Can't stop the rook, but you can pause it to allow the browser to garbage collect
-            console.log('stopping singing');
+            console.log('GAME: stopping singing');
             app.songs[key].pause();
         }
 
@@ -104,13 +102,16 @@ class PlayCardsGame extends Game {
         console.log('GAME: This teams birds: ', birds);
         let lastBird = Bird.findByID(birds[birds.length - 2]);
 
-        console.log('GAME: Comparing ' + this.currentBird.commonName + ' with ' + lastBird.commonName);
+        console.log('GAME: Comparing ' + this.currentBird.commonName + ' with ' + lastBird.commonName, [this.currentBird.sightings, lastBird.sightings]);
         let answer = this.currentBird.sightings > lastBird.sightings;
         
         if (answer === this.higherLower){
             // correct
+        } else if(this.currentBird.sightings === lastBird.sightings){
+            alert('Nothing for a pair');
+            this.changeTeam(this.currentTeam);
         } else {
-            alert('incorrect');
+            alert('Wrong');
             this.changeTeam(this.currentTeam);
         }
 
@@ -148,20 +149,32 @@ class PlayCardsGame extends Game {
     changeTeam(team){
         console.log('GAME: Change team');
         this.hasSelected = false;
+        
+        if (this.round[team - 1] > 0) {
+            let el = document.getElementById('team-' + team + '-card-' + (this.round[team - 1] - 1));
+            console.log(el);
+            el.innerHTML = "";
+            this.round[team - 1]--;
+
+            if (team === 1){
+                this.teamOneBirds.pop();
+            } else {
+                this.teamTwoBirds.pop();
+            }
+        }
+
         if (team === 1){
             this.updateTeam(2);
         } else {
             this.updateTeam(1);
         }
 
-        console.log('GAME:this.round');
-        console.log(this.currentTeam);
-        console.log(team);
+        console.log('GAME: Round, current & last team', [this.round, this.currentTeam, team]);
         if (this.round[this.currentTeam -1] === 1){
             console.log('calling starter bird');
             this.starterBird();
         }
-
+        
         return this.currentTeam;
     }
 
